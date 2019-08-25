fbd544081976b9668b303dbab641c70b13fc72c4
Nico Guedes <nico.guedes@ACTIVEMIXBUTTER.workdayinternal.com>
2019-08-25 19:03:22 +0100

    schema changes

--- app/models/person.model.js
+++ app/models/person.model.js
@@ -11,10 +11,25 @@ const EventParticipationSchema = mongoose.Schema({
 
 const PersonSchema = mongoose.Schema({
     name: String,
-    dob: String,
+    dob: Date,
+    phone: String,
+    email: String,
+    facebookId: String,
+    instagramId: String,
     eventParticipations: [EventParticipationSchema]
 }, {
-    timestamps: true
-});
+        timestamps: true
+    });
+
+// TODO: insert an index on the name field and possibly on the eventParticipation name as well
+// PersonSchema.index({
+//     name: 'text',
+//     description: 'text',
+// }, {
+//         weights: {
+//             name: 5,
+//             description: 1,
+//         },
+//     });
 
 module.exports = mongoose.model('Person', PersonSchema);
