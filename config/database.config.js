14b708c0e4e2590626e4305abf4ac02917f349b3
Nico Guedes <nico.guedes@ACTIVEMIXBUTTER.workdayinternal.com>
2019-09-08 16:03:15 +0100

    Modifying DB configuration

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: process.env.NODE_ENV == 'development' ? 'mongodb://localhost:27017' : 'mongodb://nico:kbulos0@ds211368.mlab.com:11368/heroku_wjfp4zfr'
+    url: 'mongodb://localhost:27017'
 }
